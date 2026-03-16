1f98e711427a6dc80b6466f534c88031ac7745d2
Novogrey1 <olexandrplay228666@gmail.com>
2026-03-16 12:45:55 +0100

    Optimization Part 1

--- js/consent-config.js
+++
@@ -1,6 +0,0 @@
-window.TRP_CONSENT_CONFIG = Object.assign({}, window.TRP_CONSENT_CONFIG, {
-  // Change revision to force every visitor to accept again.
-  revision: '2025-03-16',
-  storageKey: 'trp-consent-state',
-  uiLangKey: 'trp-consent-ui-lang'
-});
